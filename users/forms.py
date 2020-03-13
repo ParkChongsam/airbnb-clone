19e1b7de370dd04c1151b15e491ad46ef1d628f0
clark <inservice2@naver.com>
2020-03-13 11:15:21 +0900

    #15.0 Signup From

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from . import models
 
+
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
-    #widget=forms.PasswordInput - 패스워드가 * 표시로만 보이는 파라미터
+    # widget=forms.PasswordInput - 패스워드가 * 표시로만 보이는 파라미터
 
-    def clean(self): #여기서 매서드 이름은 clean을 반드시 해준다. 
-        email = self.cleaned_data.get('email')
-        password = self.cleaned_data.get('password')
+    def clean(self):  # 여기서 매서드 이름은 clean을 반드시 해준다.
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
         try:
             user = models.User.objects.get(email=email)
             if user.check_password(password):
@@ -27,8 +28,15 @@ class LoginForm(forms.Form):
             # raise forms.ValidationError("User does not exist")
 
 
+class SignupForm(forms.Form):
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
 
+# 장고는 username을 중요하게 생각함.생각함
+# username을 email인 것처럼 사용해서 appplication을 만든다. - user중복 체크하기 위함.
 
-#장고는 username을 중요하게 생각함.생각함
-#username을 email인 것처럼 사용해서 appplication을 만든다. - user중복 체크하기 위함.
--- users/views.py
+++ users/views.py
@@ -12,6 +12,7 @@ class LoginView(FormView):
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")  # reverse는 core:home로 가서 실제 url을 주는 매서드
 
+    # 기존 이메일과 패스워드가 있는지 확인하기(validating)
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
@@ -24,3 +25,35 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignupView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignupForm
+    success_url = reverse_lazy("core:home")  # reverse는 core:home로 가서 실제 url을 주는 매서드
+    initial = {
+        "first_name": "park",
+        "last_name": "sam",
+        "email": "inkservice2@naver.com",
+    }
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get("email")
+            # email이 있는지 확인해서 있다면 아래코드(raise)를 실행하기
+            raise froms.ValidationError("User already exists")
+        except models.User.DoesNotExist:
+            # models.User.DoesNotExist 이 에러 발생시 email을 입력한 이메일을 반환한다(사용한다)
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
