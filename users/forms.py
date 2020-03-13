f3028bc3adaa7c54c6b4a18d84a80cb3f295eee5
clark <inservice2@naver.com>
2020-03-13 13:17:28 +0900

    #15.0 Signup From

--- users/forms.py
+++ users/forms.py
@@ -36,7 +36,25 @@ class SignupForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
+    # 장고는 username을 중요하게 생각함.생각함
+    # username을 email인 것처럼 사용해서 appplication을 만든다. - user중복 체크하기 위함.
 
-# 장고는 username을 중요하게 생각함.생각함
-# username을 email인 것처럼 사용해서 appplication을 만든다. - user중복 체크하기 위함.
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get("email")
+            # email이 있는지 확인해서 있다면 아래코드(raise)를 실행하기
+            raise froms.ValidationError("User already exists")
+        except models.User.DoesNotExist:
+            # models.User.DoesNotExist 이 에러 발생시 email을 입력한 이메일을 반환한다(사용한다)
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
 
--- users/views.py
+++ users/views.py
@@ -37,23 +37,3 @@ class SignupView(FormView):
         "last_name": "sam",
         "email": "inkservice2@naver.com",
     }
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get("email")
-            # email이 있는지 확인해서 있다면 아래코드(raise)를 실행하기
-            raise froms.ValidationError("User already exists")
-        except models.User.DoesNotExist:
-            # models.User.DoesNotExist 이 에러 발생시 email을 입력한 이메일을 반환한다(사용한다)
-            return email
-
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
-
-        if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
-
